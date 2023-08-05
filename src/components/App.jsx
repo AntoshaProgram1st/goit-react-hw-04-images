1c1180a0f812f9327689210aaf197301f497a730
unknown <antoxa1012@gmail.com>
2023-08-05 15:19:56 +0300

    25

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,8 +5,8 @@ import { ToastContainer } from 'react-toastify';
 import css from './App.module.css';
 import { toast } from 'react-toastify';
 import { fetchPics } from '../components/Api/Api';
-import { Loader } from './Loader/Loader';
-import { Button } from './Button/Button';
+import { Loader } from 'components/Loader/Loader';
+import { Button } from 'components/Button/Button';
 
 export const App = () => {
   const [matches, setMatches] = useState([]);
