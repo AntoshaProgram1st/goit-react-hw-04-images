a0a312ad48d8174b1ac8a1eb67877b144973aed5
unknown <antoxa1012@gmail.com>
2023-08-05 15:50:40 +0300

    30

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,37 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 import css from './Modal.module.css';
-import { createPortal } from "react-dom";
-
-const modalRoot = document.querySelector('#modal-root');
 
 const Modal = ({ children, onClose }) => {
-    useEffect(() => {
-        const handleKeyDown = (e) => {
-            if (e.code === 'Escape') {
-                onClose();
-            }
-        }
-        window.addEventListener('keydown', handleKeyDown);
-        
-        return () => {
-            window.removeEventListener('keydown', handleKeyDown);
-        }
-    }, [onClose]);
-
+  useEffect(() => {
+    const handleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
     const handleBackdropClick = event => {
-        if (event.currentTarget === event.target) {
-            onClose();
-        }
-    }
+      if (event.currentTarget === event.target) {
+        onClose();
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
 
-    return createPortal(
-        <div className={css.overlay} onClick={handleBackdropClick}>
-            <div className={css.modal}>
-                {children}
-            </div>
-        </div>,
-        modalRoot
-    );
-}
- 
+  return (
+    <div className={css.overlay} onClick={handleBackdropClick}>
+      <div className={css.modal}>{children}</div>
+    </div>
+  );
+};
+
 export default Modal;
