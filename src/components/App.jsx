f2bfb736f11b86a08a41c6d5d4334ea08d5016ce
unknown <antoxa1012@gmail.com>
2023-08-05 15:17:55 +0300

    23

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Searchbar } from 'components/Searchbar/Searchbar';
-import { ImageGallery } from './ImageGallery/ImageGallery';
+import { ImageGallery } from 'components/ImageGallery/ImageGallery';
 import { ToastContainer } from 'react-toastify';
 import css from './App.module.css';
 import { toast } from 'react-toastify';
